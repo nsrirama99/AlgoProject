d00ddde4478bae62b173c00296e9f76b9fc5bd08
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2019-04-26 00:01:57 -0500

    Update token.cpp

--- connectFour/token.cpp
+++ connectFour/token.cpp
@@ -5,13 +5,41 @@ Token::Token()
     x = 0;
     y = 0;
     filled = false;
+    acii = ' ';
 }
 
 void Token::setFilled(int r, int g, int b)
 {
     //col.set(r, g, b);
     filled = true;
+}
 
+void Token::setFilled(char x)
+{
+    filled = true;
+    ascii = x;
+}
+
+void Token::draw()
+{
+	if (filled)
+	{
+        cout << ascii;
+		/*ofFill();
+		ofSetColor(col);
+		ofDrawCircle(x, y, 45);
+
+		ofNoFill();
+		ofSetColor(100);
+		ofDrawCircle(x, y, 38);*/
+	}
+	else
+	{
+        cout << ' ';
+		/*ofFill();
+		ofSetColor(200);
+		ofDrawCircle(x, y, 45);*/
+	}
 }
 
 
