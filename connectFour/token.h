e272a12d1915c1026d663fd5b27e8adee1930219
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2019-04-26 00:02:49 -0500

    Update token.h

--- connectFour/token.h
+++ connectFour/token.h
@@ -10,11 +10,13 @@ private:
     int y;
     //ofColor col;
     bool filled;
+    char ascii;
 public:
     Token();
     void setXY(int xL, int yL): x(xL), y(yL) {}
     void setFilled(int r, int g, int b);
     void draw();
+    void setFilled(char x);
 };
 
 #endif // TOKEN_H
